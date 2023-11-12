d883bbbec46b0983daa1c79d5849808e16686533
Nicolás Javier Contreras Jofre <nicolas.contrerasj@usm.cl>
2023-11-12 12:32:45 -0300

    Delete proyecto directory

--- proyecto/archivo.js
+++
@@ -1,7 +0,0 @@
-// /public/js/script.js
-document.querySelector('h1').addEventListener('click', function () {
-    alert('¡Hiciste clic en el título!');
-  });
-  
-  // Agrega tus scripts JavaScript aquí
-  
